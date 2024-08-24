628ec84891e290452fab787494954021852d7851
regand75 <regandess@gmail.com>
2024-08-24 13:28:01 +0300

    added backend

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,11 @@ const fetchBusData = async () => {
 
 const formatDate = (date) => date.toISOString().split('T')[0];
 const formatTime = (date) => date.toTimeString().split(' ')[0].slice(0, 5);
+const getTimeRemainingSeconds = (departureTime) => {
+    const now = new Date();
+    const timeDeference = departureTime - now;
+    return Math.floor(timeDeference / 1000);
+};
 
 const renderBusData = (buses) => {
     const tableBody = document.querySelector('#bus tbody');
@@ -19,13 +24,15 @@ const renderBusData = (buses) => {
     buses.forEach((bus) => {
         const row = document.createElement('tr');
         const nextDepartureDateTimeUTC = new Date(`${bus.nextDeparture.date}T${bus.nextDeparture.time}Z`,);
+        const remainingSeconds = getTimeRemainingSeconds(nextDepartureDateTimeUTC);
+        const remainingTimeText = remainingSeconds < 60 ? 'Отправляется' : bus.nextDeparture.remaining;
 
         row.innerHTML = `
         <td>${bus.busNumber}</td>
         <td>${bus.startPoint} - ${bus.endPoint}</td>
         <td>${formatDate(nextDepartureDateTimeUTC)}</td>
         <td>${formatTime(nextDepartureDateTimeUTC)}</td>
-        <td>${bus.nextDeparture.remaining}</td>
+        <td>${remainingTimeText}</td>
         `;
         tableBody.append(row);
     });
@@ -51,10 +58,19 @@ const initWebSocket = () => {
     })
 };
 
+const updateTime = () => {
+    const currentTimeElement = document.getElementById('current-time');
+    const now = new Date();
+    currentTimeElement.textContent = now.toTimeString().split(' ')[0];
+
+    setTimeout(updateTime, 1000);
+};
+
 const init = async () => {
     const buses = await fetchBusData();
     renderBusData(buses);
     initWebSocket();
+    updateTime();
 };
 
 init();
